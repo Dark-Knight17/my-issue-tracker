ca2195c94b5e0b5d3499a18f946f10adb061502d
Israel Pedro <pedroisrael980@gmail.com>
2025-05-14 14:33:30 +0100

    Remove id type validation logic.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/prisma/client";
+import { Heading } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -6,7 +7,6 @@ interface Props {
   params: { id: string };
 }
 const IssueDetailPage = async ({ params }: Props) => {
-  if(typeof params.id !== 'number') notFound();
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
@@ -16,7 +16,7 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
   return (
     <div>
-      <p>{issue.title}</p>
+      <Heading>{issue.title}</Heading>
       <p>{issue.description}</p>
       <p>{issue.status}</p>
       <p>{issue.createdAt.toDateString()}</p>
