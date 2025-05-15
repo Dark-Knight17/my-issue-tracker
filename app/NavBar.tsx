8e8c6c8e08bf8b2a026aa7084fc9228216625ebc
Israel Pedro <pedroisrael980@gmail.com>
2025-05-15 13:02:22 +0100

    Refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,75 +1,80 @@
 "use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-import { FaBug } from "react-icons/fa";
+import { Avatar, Box, Container, DropdownMenu, Flex } from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Avatar, Box, Container, DropdownMenu, Flex } from "@radix-ui/themes";
-
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const links = [
-    { label: "dashboard", href: "/" },
-    { label: "issues", href: "/issues" },
-  ];
-
-  const { data: session, status } = useSession();
-
   return (
-    <nav
-      className={`mb-5 px-5 py-3 border-b`}
-    >
+    <nav className={`mb-5 px-5 py-3 border-b`}>
       <Container>
         <Flex justify="between">
           <Flex align="center" gap="3">
             <Link href="/">
               <FaBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li
-                  className={classNames({
-                    "text-zinc-100": currentPath === link.href,
-                    "text-zinc-500": currentPath !== link.href,
-                    "hover:text-white transition-colors": true,
-                    "font-bold": true,
-                  })}
-                  key={link.href}
-                >
-                  <Link href={link.href}>{link.label}</Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>{session.user!.email}</DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">signout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">signin</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const AuthStatus = () => {
+  const { data: session, status } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Link href="/api/auth/signin">signin</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>{session!.user!.email}</DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">signout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "dashboard", href: "/" },
+    { label: "issues", href: "/issues" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li
+          className={classNames({
+            "text-zinc-100": currentPath === link.href,
+            "text-zinc-500": currentPath !== link.href,
+            "hover:text-white transition-colors": true,
+            "font-bold": true,
+          })}
+          key={link.href}
+        >
+          <Link href={link.href}>{link.label}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
