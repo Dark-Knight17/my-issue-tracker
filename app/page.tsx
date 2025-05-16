9a16b3f55b522dece4655779a8fdcb41b419761f
Israel Pedro <pedroisrael980@gmail.com>
2025-05-16 16:59:39 +0100

    Build the lastest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Pagination from "./components/Pagination";
+import LastestIssues from "./LastestIssues";
 
 export default async function Home(props: {
   searchParams: Promise<{ page: string }>;
 }) {
   const searchParams = await props.searchParams;
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+  return <LastestIssues />;
 }
