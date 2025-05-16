0810839087568502235fc3e7d0492237fb76bb89
Israel Pedro <pedroisrael980@gmail.com>
2025-05-16 20:07:00 +0100

    Refactor: enhance layout of issueDetail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/prisma/client";
-import { Box, Flex, Grid } from "@radix-ui/themes";
+import { Box, Container, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
@@ -24,26 +24,28 @@ const IssueDetailPage = async (props: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid
-      columns={{
-        initial: "1",
-        sm: "5",
-      }}
-      gap="5"
-    >
-      <Box className="md:col-span-4">
-        <IssueDetails issue={issue} />
-      </Box>
-      {session && (
-        <Box>
-          <Flex direction="column" gap="4">
-            <AssigneeSelect issue={issue} />
-            <EditIssueButton issueId={issue.id} />
-            <DeleteIssueBUtton issueId={issue.id} />
-          </Flex>
+    <Container>
+      <Grid
+        columns={{
+          initial: "1",
+          sm: "5",
+        }}
+        gap="5"
+      >
+        <Box className="md:col-span-4">
+          <IssueDetails issue={issue} />
         </Box>
-      )}
-    </Grid>
+        {session && (
+          <Box>
+            <Flex direction="column" gap="4">
+              <AssigneeSelect issue={issue} />
+              <EditIssueButton issueId={issue.id} />
+              <DeleteIssueBUtton issueId={issue.id} />
+            </Flex>
+          </Box>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
