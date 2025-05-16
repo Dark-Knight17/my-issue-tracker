f6f5fb3abe8f5daed61087a0617d41b14d21fd3d
Israel Pedro <pedroisrael980@gmail.com>
2025-05-16 16:18:26 +0100

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -8,6 +8,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -18,22 +19,47 @@ interface Props {
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
   return (
     <Flex align="center" gap="2">
       <Text size="1">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
