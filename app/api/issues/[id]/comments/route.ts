3ff8e1a98092b8d0d33b5cc4543897bad7d364c0
Israel Pedro <pedroisrael980@gmail.com>
2025-05-18 10:03:22 +0100

    Create API route for posting comments.

---
+++ app/api/issues/[id]/comments/route.ts
@@ -0,0 +1,21 @@
+import { commentSchema } from "@/app/validationSchemas";
+import { auth } from "@/auth";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const session = await auth();
+  if (!session) return NextResponse.json({}, { status: 401 });
+
+  const body = await request.json();
+  const validation = commentSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const newComment = await prisma.comment.create({
+    data: validation.data,
+  });
+
+  return NextResponse.json(newComment, { status: 201 });
+}
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,15 +1,31 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-    title: z.string().min(1, 'Title is required').max(255),
-    description: z.string().min(1,'Description is required').max(65535),
-    status: z.enum(["OPEN", "CLOSED", "IN_PROGRESS"]).optional()
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required").max(65535),
+  status: z.enum(["OPEN", "CLOSED", "IN_PROGRESS"]).optional(),
 });
-
 
 export const patchIssueSchema = z.object({
-    title: z.string().min(1, 'Title is required').max(255).optional(),
-    description: z.string().min(1,'Description is required').max(65535).optional(),
-    status: z.enum(["OPEN", "CLOSED", "IN_PROGRESS"]).optional(),
-    assignedToUserId: z.string().min(1,"AssignedToUserId is required.").max(255).optional().nullable()
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+  status: z.enum(["OPEN", "CLOSED", "IN_PROGRESS"]).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
+
+export const commentSchema = z
+  .object({
+    comment: z.string().trim().min(1, "Comment is required").max(255),
+    issueId: z.number().int("Issue ID must be an integer"),
+    authorId: z.string().min(1, "AuthorId is required").max(255),
+  })
+  .strict();
